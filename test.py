d04d1f9a8d2a6391ee15a75a35eb02e9fb15bbd7
azaliafd <azaliafathimah@gmail.com>
2025-10-07 09:22:08 +0700

    tampilkan nama bunga pada hasil prediksi di test.py

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import bentoml
+from sklearn import datasets
 
 runner = bentoml.sklearn.get("iris_svm_model:latest").to_runner()
 runner.init_local()
 
 sample = [[5.9, 3.0, 5.1, 1.8]]
+
 prediction = runner.predict.run(sample)
 
-print(f"Hasil prediksi untuk {sample}: {prediction}")
+iris = datasets.load_iris()
+predicted_label = iris.target_names[prediction[0]]
+
+print("Input:", sample)
+print(f"Hasil Prediksi: {predicted_label} (kelas {prediction[0]})")
